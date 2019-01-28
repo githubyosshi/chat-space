b69f423c674fd8d49e934b70e8070479014538af
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-01-28 13:11:25 +0900

    Incremental_search_add_7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
   def index
     @users = User.where('name LIKE(?)', "%#{params[:name]}%").where.not(id: current_user)
     respond_to do |format|
-      format.html
       format.json
     end
   end
